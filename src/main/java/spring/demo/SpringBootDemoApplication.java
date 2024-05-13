67e3eda8e58e8bc538ed04b099b321f9918a9c8f
Anton T <antont@stacc.com>
2024-05-13 15:47:58 +0200

    rename application file

--- src/main/java/spring/demo/SpringBootDemoApplication.java
+++ src/main/java/spring/demo/SpringBootDemoApplication.java
@@ -1,14 +1,15 @@
 package spring.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
-@org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootApplication {
+@SpringBootApplication
+public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApplication.class, args);
+		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
 }
--- src/test/java/spring/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/spring/demo/SpringBootDemoApplicationTests.java
@@ -3,8 +3,8 @@ package spring.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SpringBootApplicationTests.class)
-class SpringBootApplicationTests {
+@SpringBootTest(classes = SpringBootDemoApplicationTests.class)
+class SpringBootDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
